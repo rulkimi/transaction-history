af25845773cfeaed82317f2b041583bf698597a9
rulkimi <azrulkimi00@gmail.com>
2026-03-14 16:32:54 +0800

    add darkmode for header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,9 +5,25 @@ import "./global.css";
 export default function RootLayout() {
   return (
     <ThemeProvider>
-      <Stack>
-        <Stack.Screen name="index" options={{ title: "Home" }} />
-        <Stack.Screen name="transactions" options={{ title: "Transactions" }} />
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "var(--background)",
+          },
+          headerTintColor: "var(--foreground)",
+        }}
+      >
+        <Stack.Screen 
+          name="index" 
+          options={{ 
+            title: "Home", 
+            headerShown: false
+          }} 
+        />
+        <Stack.Screen
+          name="transactions" 
+          options={{ title: "Transactions" }} 
+        />
       </Stack>
     </ThemeProvider>
   );
